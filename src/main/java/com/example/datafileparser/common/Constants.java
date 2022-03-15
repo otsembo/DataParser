616e30f97db613beccfca6fc633379d8f236f66d
otsembo <okumu.otsembo@gmail.com>
2022-03-15 13:15:03 +0300

    Application: Added download links to the home page

--- src/main/java/com/example/datafileparser/common/Constants.java
+++ src/main/java/com/example/datafileparser/common/Constants.java
@@ -10,4 +10,6 @@ public class Constants {
 
     public static final ArrayList<User> DEFAULT_USERS = new ArrayList<>();
 
+    public static final String FILE_DOWNLOAD_URL = "https://drive.google.com/file/d/1I2vFp79Sg_X3jPMH5o4keQDPfx2lU1k9/view?usp=sharing";
+
 }
--- src/main/java/com/example/datafileparser/presentation/controllers/HomeController.java
+++ src/main/java/com/example/datafileparser/presentation/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.datafileparser.presentation.controllers;
 
+import com.example.datafileparser.common.Constants;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ public class HomeController {
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("name", "data");
+        model.addAttribute("download_url", Constants.FILE_DOWNLOAD_URL);
         return "index";
     }
 
